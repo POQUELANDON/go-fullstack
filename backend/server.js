58a64c7393e6d91904aa467ae9a731bd54268076
Natacha POQUE-LANDON <natacha.poque@hotmail.fr>
2023-11-24 11:06:17 +0100

    Création serveur Express simple : Configuration environnement développement. Démarrage serveur Node. Création application Express Création route GET Création route POST

---
+++ backend/server.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const app = require('./app');
+
+// Fonction pour normaliser le port
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+
+// Récupération du port à utiliser
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// Fonction pour gérer les erreurs
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+// Création du serveur HTTP
+const server = http.createServer(app);
+
+// Gestionnaire d'erreurs
+server.on('error', errorHandler);
+
+// Démarrage du serveur
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+});
+
+// Écoute du port
+server.listen(port);
